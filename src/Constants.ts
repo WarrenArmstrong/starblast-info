0cb1d87b1b5b89471036ce7bb06ce48b9b717f67
Paul Armstrong <paulbarmstrong@hotmail.com>
2022-10-10 01:34:30 -0400

    raise system info fetch frequency

--- src/Constants.ts
+++ src/Constants.ts
@@ -7,7 +7,7 @@ export default {
 	teamColorLightness: 50,
 	maxLobbyCacheTimeMs: 2 * 60 * 1000,
 	simStatusFetchFrequencyMs: 10 * 1000,
-	systemInfoFetchFrequencyMs: 1000,
+	systemInfoFetchFrequencyMs: 5 * 1000,
 	systemInfoNonDaemonFetchFrequencyMs: 10 * 1000,
 	systemInfoEndpoint: "https://z4ji7p36rc.execute-api.us-east-2.amazonaws.com/Prod"
 }
